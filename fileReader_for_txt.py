2d9e4546822b4505df0e56c4709cf92fe56b6ae7
Gingrefel Constante <ging18tucson@hotmail.com>
2016-09-20 19:25:25 -0700

    python file text reader

---
+++ fileReader_for_txt.py
@@ -0,0 +1,18 @@
+# fileReader for .txt
+# this demonstrates how to read a file and 
+# parse it using python
+
+##################### comment each line please ########################
+def main():
+    # this will read the file
+    file = open("Elements.txt", "r") # r means to read the file
+    # readlines will also process each line separately 
+    lines = file.readlines() # this will read all the lines in the document not just a single line
+    file.close() # make sure to always close the file
+
+    # to look for patterns
+    for everyline in lines:
+        everyline = everyline.strip() # this will remove the extra spaces between lines
+        print (everyline)
+    # display result 
+main()
