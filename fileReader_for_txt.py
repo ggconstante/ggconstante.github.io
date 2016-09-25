b74d7e8fac200e4c9ff28d7a4e6057b9ea214b6d
areiber <areiber125@gmail.com>
2016-09-25 14:47:22 -0700

    updated

--- fileReader_for_txt.py
+++ fileReader_for_txt.py
@@ -1,7 +1,7 @@
 # fileReader for .txt
 # this demonstrates how to read a file and 
 # parse it using python
-
+# from HTMLParser import html.parser
 ##################### comment each line please ########################
 def main():
     # this will read the file
@@ -10,9 +10,23 @@ def main():
     lines = file.readlines() # this will read all the lines in the document not just a single line
     file.close() # make sure to always close the file
 
+    html = open("test.html", "w")
+    html.write()
+
     # to look for patterns
     for everyline in lines:
         everyline = everyline.strip() # this will remove the extra spaces between lines
-        print(everyline)
+        # print(everyline)
+
+        if everyline.startswith("##"):
+            everyline = everyline[3:]
+            taglist = everyline.split(",")
+            htmlitem = taglist[0]
+            print(htmlitem)
+
+
+            
+
+
     # display result 
 main()
